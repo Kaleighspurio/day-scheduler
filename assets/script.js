b22bf668f14868e5fe4e59762a1d161e1cce533d
Kaleigh Spurio <kaleighastell@gmail.com>
2020-07-30 11:43:16 -0400

    cleaned up a few things, replaced var

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,27 @@
-var eventInput9AM;
-var eventInput10AM;
-var eventInput11AM;
-var eventInput12PM;
-var eventInput1PM;
-var eventInput2PM;
-var eventInput3PM;
-var eventInput4PM;
-var eventInput5PM;
-var eventInput6PM;
-var eventInput7PM;
-var events;
-var eventObject;
+let eventInput9AM;
+let eventInput10AM;
+let eventInput11AM;
+let eventInput12PM;
+let eventInput1PM;
+let eventInput2PM;
+let eventInput3PM;
+let eventInput4PM;
+let eventInput5PM;
+let eventInput6PM;
+let eventInput7PM;
+let events;
+let eventObject;
 
 // cited https://stackoverflow.com/questions/10590461/dynamic-date-and-time-with-moment-js-and-setinterval
 // when the page opens, the date and time are displayed at the top
-var dateAndTime = null,
+let dateAndTime = null,
         date = null;
 // this updates the date and time with the current date.time
-var update = function () {
+const update = function () {
     date = moment(new Date())
     dateAndTime.html(date.format('dddd, MMMM Do YYYY, h:mm a'));
 };
-$(document).ready(function(){
+$(document).ready(() => {
     dateAndTime = $(".display-date")
     // this calls the update function written above
     update();
@@ -53,38 +53,25 @@ $(".save-btn").on("click", saveEvents);
 
 function saveEvents(event){
     event.preventDefault();
-    // These assign the value of what the user typed in the text area to variables
-    eventInput9AM = $(".9-am").val();
-    eventInput10AM = $(".10-am").val();
-    eventInput11AM = $(".11-am").val();
-    eventInput12PM = $(".12-pm").val();
-    eventInput1PM = $(".1-pm").val();
-    eventInput2PM = $(".2-pm").val();
-    eventInput3PM = $(".3-pm").val();
-    eventInput4PM = $(".4-pm").val();
-    eventInput5PM = $(".5-pm").val();
-    eventInput6PM = $(".6-pm").val();
-    eventInput7PM = $(".7-pm").val();
-    console.log(eventInput11AM);
     // Object is created with the values of the textareas to be stored in local storage
     eventObject = {
-        nineAM: eventInput9AM,
-        tenAM: eventInput10AM,
-        elevenAM: eventInput11AM,
-        twelvePM: eventInput12PM,
-        onePM: eventInput1PM,
-        twoPM:eventInput2PM,
-        threePM: eventInput3PM,
-        fourPM: eventInput4PM,
-        fivePM: eventInput5PM,
-        sixPM: eventInput6PM,
-        sevenPM: eventInput7PM
+        nineAM: $(".9-am").val(),
+        tenAM: $(".10-am").val(),
+        elevenAM: $(".11-am").val(),
+        twelvePM: $(".12-pm").val(),
+        onePM: $(".1-pm").val(),
+        twoPM: $(".2-pm").val(),
+        threePM: $(".3-pm").val(),
+        fourPM: $(".4-pm").val(),
+        fivePM: $(".5-pm").val(),
+        sixPM: $(".6-pm").val(),
+        sevenPM: $(".7-pm").val()
     }
     console.log(eventObject);
     // This gets whatever is already saved in local storage, and then adds onto it
-    var events = getEventsFromLocalStorage();
+    let events = getEventsFromLocalStorage();
     events.push(eventObject);
-    var eventsJSON = JSON.stringify(events);
+    let eventsJSON = JSON.stringify(events);
     localStorage.setItem("events", eventsJSON);
 }
 // get any data that is already saved in local storage
@@ -98,14 +85,12 @@ function getEventsFromLocalStorage(){
 }
 // *********Elapsed time color change******
 // Depending on the current time, events that are past, present or future will be displayed as different colors.
- var m = moment(new Date);
- console.log(m.format("HH"));
+ const m = moment(new Date);
 //  grab the data from the text areas, compare it with the current hour
 
 // This is an array of the classes for my textarea elements
-var textareaArray = [$(".9-am"), $(".10-am"), $(".11-am"), $(".12-pm"), $(".1-pm"), $(".2-pm"), $(".3-pm"), $(".4-pm"), $(".5-pm"), $(".6-pm"), $(".7-pm")]
-console.log(textareaArray)
-textareaArray.forEach(function(time){
+const textareaArray = [$(".9-am"), $(".10-am"), $(".11-am"), $(".12-pm"), $(".1-pm"), $(".2-pm"), $(".3-pm"), $(".4-pm"), $(".5-pm"), $(".6-pm"), $(".7-pm")]
+textareaArray.forEach((time) => {
     // for each textarea element in my array, grab the data attribute
     dataHour = time.attr("data-input");
     console.log(dataHour);
@@ -120,9 +105,9 @@ textareaArray.forEach(function(time){
     }
 })
 
-// **************** What happens if it is a new day*********
-// There is a clear schedule button that can be clicked at the end of the day to clear the schedule
-$(".clear-button").on("click", function(){
+
+// Clear button will clear the calendar
+$(".clear-button").on("click", () => {
     // when the clear button is clicked, local storage is cleared
     localStorage.clear();
     // and the page reloads
